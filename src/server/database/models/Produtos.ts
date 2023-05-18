0a3724e472f7aeaa930cd9a298ba69a88c90d4b6
FERNANDO NUNES <fernandojqnunes@gmail.com>
2023-05-18 17:22:19 -0300

    contas okay

--- src/server/database/models/Produtos.ts
+++ src/server/database/models/Produtos.ts
@@ -9,74 +9,74 @@ export interface IProduto {
     subId?: number; //forgot subtipo
     modeloId?: number; //forgot modelo
 
-    descricaoDetalhada: string; 
-    origem: string; 
-    cfop: string; 
-    cstVendas: string; 
-    escrituracao: string; 
+    descricaoDetalhada?: string; 
+    origem?: string; 
+    cfop?: string; 
+    cstVendas?: string; 
+    escrituracao?: string; 
 
     ncmId?: number; //forgot ncm
 
-    embalagem: string; 
+    embalagem?: string; 
 
-    quantidadeEmbalagem: number; 
+    quantidadeEmbalagem?: number; 
 
-    unidade: string; 
+    unidade?: string; 
 
-    liquido: number; 
+    liquido?: number; 
 
-    bruto: number; 
+    bruto?: number; 
 
     fornecedor1Id?: number; // forgot fornecedor
     fornecedor2Id?: number; // forgot fornecedor
     fornecedor3Id?: number; // forgot fornecedor
 
-    codigoFabricante: string; 
-    dataUltimaCompra: string; 
-    nfe: string; 
+    codigoFabricante?: string; 
+    dataUltimaCompra?: string; 
+    nfe?: string; 
 
-    produtoAtivo: boolean; 
-    produtoAcabado: boolean; 
-    proprio: boolean; 
-    terceiros: boolean; 
-    receita: boolean; 
-    paraVenda: boolean; 
-    paraCompra: boolean; 
-    outro: boolean; 
-    descricaoOutro: string; 
+    produtoAtivo?: boolean; 
+    produtoAcabado?: boolean; 
+    proprio?: boolean; 
+    terceiros?: boolean; 
+    receita?: boolean; 
+    paraVenda?: boolean; 
+    paraCompra?: boolean; 
+    outro?: boolean; 
+    descricaoOutro?: string; 
 
-    promocao: boolean; 
-    moeda: string; 
-    precoCusto: number; 
-    capagem: number; 
-    precoCompra: number;
+    promocao?: boolean; 
+    moeda?: string; 
+    precoCusto?: number; 
+    capagem?: number; 
+    precoCompra?: number;
 
-    margemLucro1: number; 
-    margemLucro2: number; 
-    margemLucro3: number; 
-    margemLucro4: number; 
-    margemLucro5: number;
-    condicao1: string; 
-    condicao2: string; 
-    condicao3: string; 
-    condicao4: string; 
-    condicao5: string; 
-    precoVenda1: number; 
-    precoVenda2: number; 
-    precoVenda3: number; 
-    precoVenda4: number; 
-    precoVenda5: number;
+    margemLucro1?: number; 
+    margemLucro2?: number; 
+    margemLucro3?: number; 
+    margemLucro4?: number; 
+    margemLucro5?: number;
+    condicao1?: string; 
+    condicao2?: string; 
+    condicao3?: string; 
+    condicao4?: string; 
+    condicao5?: string; 
+    precoVenda1?: number; 
+    precoVenda2?: number; 
+    precoVenda3?: number; 
+    precoVenda4?: number; 
+    precoVenda5?: number;
 
-    ipiCompra: number; 
-    ipiVenda: number; 
-    icmsCompra: number; 
-    icmsVenda: number; 
-    pis: number; 
-    cofins: number; 
-    baseCalculoReduzida: boolean; 
-    porcentagemReducao: number; 
-    comissaoDiferenciada: boolean; 
-    porcentagemComissao: number;
+    ipiCompra?: number; 
+    ipiVenda?: number; 
+    icmsCompra?: number; 
+    icmsVenda?: number; 
+    pis?: number; 
+    cofins?: number; 
+    baseCalculoReduzida?: boolean; 
+    porcentagemReducao?: number; 
+    comissaoDiferenciada?: boolean; 
+    porcentagemComissao?: number;
     
 
     empresaId: number;
