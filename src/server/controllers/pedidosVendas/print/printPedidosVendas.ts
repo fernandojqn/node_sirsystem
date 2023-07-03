3c289983c70dd01a1e5ff36a0d75cff98301437d
FERNANDO NUNES <109233114+fernandojqn@users.noreply.github.com>
2023-07-03 09:10:41 -0300

    save

--- src/server/controllers/pedidosVendas/print/printPedidosVendas.ts
+++ src/server/controllers/pedidosVendas/print/printPedidosVendas.ts
@@ -2,27 +2,43 @@ import { Request, Response } from 'express';
 import * as pdf from 'html-pdf';
 import ejs from 'ejs';
 import path from 'path';
+import { PedidosVendasProvider } from '../../../database/providers';
+
 
 export const printPedidosVendas = async (req: Request, res: Response) => {
-    console.log(req);
-
-    const nome = 'Fernando Nunes';
-
-    const filePath = path.join(__dirname, 'print.ejs');
+    // pegar os dados do pedidos de vendas
+    const result = await PedidosVendasProvider.getById(req.body.id);
+    
+    // Vê se deu erro
+    if (result instanceof Error) {
+        console.error('Erro ao obter o pedido de vendas:', result);
+        return res.status(500).send('Erro ao obter o pedido de vendas.');
+    }
+    
+    
 
     
-    ejs.renderFile(filePath, { nome: nome }, (err, html) => {
-        if (err) {
+
+    // nome do arquivo para salvar e dar o link
+    const nomeDoArquivo = `${result.pedidoId?.toString() ?? ''}${result.empresaId?.toString() ?? ''}.pdf`;    
+    // dá o nome verdadeiro para salvar e dá o caminho para o arquivo
+    const filePath = path.join(__dirname, 'print.ejs');
+    
+    ejs.renderFile(filePath, { 
+        nome: result.clienteId
+
+    }, (err, html) => {
+        if (err) { // Erro com o print.ejs
             console.error('Erro na leitura do arquivo "print.ejs":', err);
             res.status(500).send('Erro na leitura do arquivo.');
         } else {
             const options = {}; // Opções do pdf.create() aqui
-            pdf.create(html, options).toFile('meuPDF.pdf', (err, pdfRes) => {
-                if (err) {
+            pdf.create(html, options).toFile(nomeDoArquivo, (err, pdfRes) => { // Criar o pdf
+                if (err) { // erro em criar o pdf
                     console.error('Erro na criação do arquivo PDF:', err);
                     res.status(500).send('Erro na criação do arquivo PDF.');
-                } else {
-                    const pdfPath = path.join(__dirname, 'meuPDF.pdf');
+                } else { // Enviar o link para o front
+                    const pdfPath = path.join(__dirname, nomeDoArquivo);
                     // Enviar uma resposta JSON com o caminho do arquivo
                     res.json({ filePath: pdfPath });
                 }
@@ -30,196 +46,3 @@ export const printPedidosVendas = async (req: Request, res: Response) => {
         }
     });
 };
-
-/**
-import { Request, Response } from 'express';
-import * as pdf from 'html-pdf';
-import ejs from 'ejs';
-import path from 'path';
-
-export const printPedidosVendas = async (req: Request, res: Response) => {
-    console.log(req);
-
-    const nome = 'Fernando';
-
-    const filePath = path.join(__dirname, 'print.ejs');
-
-    
-    ejs.renderFile(filePath, { nome: nome }, (err, html) => {
-        if (err) {
-            console.error('Erro na leitura do arquivo "print.ejs":', err);
-            res.status(500).send('Erro na leitura do arquivo.');
-        } else {
-            console.log(html);
-            const options = {}; // Opções do pdf.create() aqui
-            pdf.create(html, options).toFile('meuPDF.pdf', (err, pdfRes) => {
-                if (err) {
-                    console.error('Erro na criação do arquivo PDF:', err);
-                    res.status(500).send('Erro na criação do arquivo PDF.');
-                } else {
-                    const pdfPath = path.join(__dirname, 'meuPDF.pdf');
-                    res.download(pdfPath, 'meuPDF.pdf', (err) => {
-                        if (err) {
-                            console.error('Erro ao enviar o arquivo para download:', err);
-                            res.status(500).send('Erro ao enviar o arquivo para download.');
-                        } else {
-                            // Após o download exclui o arquivo
-                            // fs.unlinkSync(pdfPath);
-                        }
-                    });
-                }
-               
-            });
-        }
-    });
-    
-};
- */
-
-/**
-import { Request, Response } from 'express';
-import * as pdf from 'html-pdf';
-import ejs from 'ejs';
-import path from 'path';
-
-export const printPedidosVendas = async (req: Request, res: Response) => {
-    console.log(req);
-
-    const nome = 'Fernando';
-
-    const filePath = path.join(__dirname, 'print.ejs');
-
-    ejs.renderFile(filePath, { nome: nome }, (err, html) => {
-        if (err) {
-            console.error('Erro na leitura do arquivo "print.ejs":', err);
-            res.status(500).send('Erro na leitura do arquivo.');
-        } else {
-            console.log(html);
-            const options = {}; // Opções do pdf.create() aqui
-            pdf.create(html, options).toFile('testePDF.pdf', (err, pdfRes) => {
-                if (err) {
-                    console.error('Erro na criação do arquivo PDF:', err);
-                    res.status(500).send('Erro na criação do arquivo PDF.');
-                } else {
-                    console.log(pdfRes);
-                    res.send('PDF criado com sucesso.');
-                }
-            });
-        }
-    });
-};
-*/
-
-
-/**
-import { Request, Response, response } from 'express';
-import path from 'path';
-import ejs from 'ejs';
-import * as pdf from 'html-pdf';
-
-export const printPedidosVendas = async (req: Request, res: Response) => {
-    const passageiros = [
-        {
-            nome: 'Antonio',
-            voo: '133'
-        },
-        {
-            nome: 'Carlos',
-            voo: '134'
-        }
-    ];
-
-    const filePath = path.join(__dirname, 'print.ejs'); // Caminho para o arquivo print.ejs
-
-    ejs.renderFile(filePath, { passageiros }, (err, html) => {
-        if (err) {
-            console.error('Erro na leitura do arquivo:', err);
-            return res.send('Erro na leitura do arquivo');
-        } 
-
-        const options = {
-            height: '11.251in',
-            width: '8.1in',
-            header: {
-                height: '20mm'
-            },
-            footer: {
-                height: '20mm'
-            }
-        };
-
-        // criar o pdf
-        pdf.create(html, options).toFile('report.pdf', (err, data) => {
-            if (err) {
-                console.error('Erro ao gerar o pdf:', err);
-                return res.send('Erro ao gerar o pdf');
-            }
-            // enviar para o front
-            return response.send(html);
-        });
-
-        // enviar para o front
-        return response.send(html);
-
-        // Use o conteúdo renderizado do arquivo 'print.ejs' conforme necessário
-        console.log('Conteúdo renderizado:', html);
-    });    
-};
-*/
-
-/**
-import { Request, Response } from 'express';
-import PdfPrinter from 'pdfmake';
-import { TDocumentDefinitions } from 'pdfmake/interfaces';
-import fs from 'fs';
-
-export const printPedidosVendas = async (req: Request, res: Response) => {
-    try {
-        const fonts = {
-            Roboto: {
-                normal: 'fonts/Roboto-Regular.ttf',
-                bold: 'fonts/Roboto-Medium.ttf',
-                italics: 'fonts/Roboto-Italic.ttf',
-                bolditalics: 'fonts/Roboto-MediumItalic.ttf'
-            }
-        };
-
-        const printer = new PdfPrinter(fonts);
-
-        // Construir formulário
-        const docDefinitions: TDocumentDefinitions = {
-            defaultStyle: { font: 'Roboto' },
-            content: [
-                { text: 'Meu vendas relatório' },
-            ]
-        };
-
-        const pdfDoc = printer.createPdfKitDocument(docDefinitions);
-
-        const chunks: Buffer[] = [];
-
-        pdfDoc.on('data', (chunk) => {
-            chunks.push(chunk);
-        });
-
-        pdfDoc.on('end', () => {
-            const result = Buffer.concat(chunks);
-            
-            // Enviar o PDF como download
-            res.setHeader('Content-Disposition', 'attachment; filename="relatorio.pdf"');
-            res.setHeader('Content-Type', 'application/pdf');
-            res.send(result);
-
-            // Ou abrir o PDF em uma nova aba
-            res.setHeader('Content-Type', 'application/pdf');
-            res.send(result);
-        });
-
-        pdfDoc.end();
-        
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Erro ao imprimir nota fiscal');
-    }
-};
- */
